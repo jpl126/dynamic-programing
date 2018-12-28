68d3cd23ef50391bff2855f601b2b0fc46468075
Jacek Plocharczyk <jacek.plocharczyk@greensteam.com>
2018-12-28 12:43:14 +0100

    removing old files

--- utils/ecosystem.py
+++
@@ -1,8 +0,0 @@
-from .agent import Agent
-from .environment import Environment
-
-
-class Ecosystem:
-    def __init__(self, agent: Agent, environment: Environment):
-        self.agent = agent
-        self.environment = environment
