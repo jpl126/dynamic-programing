16262dcba5468431479ed459396088eb17b82eeb
jpl126 <jplo@protonmail.com>
2019-01-08 19:13:49 +0100

    run_example small refactor

--- dynamic_programming/run_example.py
+++ dynamic_programming/run_example.py
@@ -7,13 +7,13 @@ if __name__ == '__main__':
     print('Choose example:\n1 - Iterative Policy Evaluation\n'
           '2 - Policy Iteration\n3 - Value Iteration')
     examples = {
-        1: frozen_lake_iterative_policy_evaluation,
-        2: frozen_lake_policy_iteration,
-        3: frozen_lake_value_iteration
+        '1': frozen_lake_iterative_policy_evaluation,
+        '2': frozen_lake_policy_iteration,
+        '3': frozen_lake_value_iteration
     }
-    decision = int(input('Your choice: '))
+    decision = input('Your choice: ')
 
-    if decision in set(examples):
+    if decision in examples:
         examples[decision].main()
     else:
         print('Wrong example. Quiting...')
